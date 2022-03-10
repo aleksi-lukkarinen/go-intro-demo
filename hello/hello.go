aa3c31ac47b3344be9b3f030baa629d32114aa99
Aleksi Lukkarinen <aleksi.lukkarinen@gmail.com>
2022-03-10 14:45:23 +0200

    Adds a function for multiple greetings

--- hello/hello.go
+++ hello/hello.go
@@ -11,7 +11,9 @@ func main() {
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
 
-	message, err := greetings.Hello("Aleksi")
+	names := []string{"Aleksi", "Giàu", "Lucky"}
+
+	message, err := greetings.Hellos(names)
 	if err != nil {
 		log.Fatal(err)
 	}
